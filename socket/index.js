4d41c09b14790dcddd14931118625c218f1a3995
readul-islam <developer.readul@gmail.com>
2023-07-23 21:03:21 +0600

    update

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server(9000, {
   cors: true,
-  origins: ["http://127.0.0.1:5173"],
+  origins: ["https://magenta-froyo-758119.netlify.app/"],
 });
 
 
